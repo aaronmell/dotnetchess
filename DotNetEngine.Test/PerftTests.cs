571583232eafa8a63df9fb1c79fdcef2e3fc7e5a
Aaron Mell <aaronamell@gmail.com>
2014-09-16 07:56:10 -0500

    Adding Divide Test to find root cause of perft failures

--- DotNetEngine.Test/PerftTests.cs
+++ DotNetEngine.Test/PerftTests.cs
@@ -23,30 +23,43 @@ namespace DotNetEngine.Test
         private long numberOfChecks = 0;
         private long checkmates = 0;
 
+        private string DivideOutput = "Move Nodes" + Environment.NewLine;
+
         [TestCase("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 0, 1)]
         [TestCase("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 1, 20)]
         [TestCase("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 2, 400)]
         [TestCase("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 3, 8902)]
         [TestCase("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 4, 197281)]
         [TestCase("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 5, 4865609)]
+        //[TestCase("rnbqkbnr/ppppp2p/8/5PpQ/8/8/PPPP1PPP/RNB1KBNR b KQkq - 0 3", 1, 1)]
         public void RunPerft(string fen, int depth, long moveCount)
         {
             var gameState = GameStateUtility.LoadGameStateFromFen(fen);
 
             var count = RunPerftRecursively(gameState, 1, depth);
             Assert.That(count, Is.EqualTo(moveCount));
-
         }
 
-        private int RunPerftRecursively(GameState gameState, int ply, int depth)
-        {           
+        [TestCase("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 5)]
+        //[TestCase("rnbqkbnr/pppppppp/8/8/8/2N5/PPPPPPPP/R1BQKBNR b KQkq - 1 1", 4)]
+        //[TestCase("rnbqkbnr/p1pppppp/8/1p6/8/N7/PPPPPPPP/R1BQKBNR w KQkq b6 0 2", 4)] //B7B5 Divide
+        //[TestCase("rnbqkbnr/p1pppppp/8/1p6/2N5/8/PPPPPPPP/R1BQKBNR b KQkq - 1 2", 5)] //A3C4 Divide
+        //[TestCase("rnbqkbnr/p1pppppp/8/8/2p5/8/PPPPPPPP/R1BQKBNR w KQkq - 0 3", 5)]
+        public void RunDivide(string fen, int depth)
+        {
+            var gameState = GameStateUtility.LoadGameStateFromFen(fen);
 
+            var result = CalculateDivide(gameState, 1, depth);            
+        }
+
+        private ulong RunPerftRecursively(GameState gameState, int ply, int depth)
+        {
             if (depth == 0)
             {
                 return 1;
             }
 
-            var count = 0;
+            ulong count = 0;
 
             gameState.GenerateMoves(MoveGenerationMode.All, ply, _moveData);
 
@@ -57,28 +70,55 @@ namespace DotNetEngine.Test
                 if (!gameState.IsOppositeSideKingAttacked(_moveData))
                 {
                     count += RunPerftRecursively(gameState, ply + 1, depth - 1);
-
-                    if (move.IsPieceCaptured())
-                        numberOfCaptures++;
-                    if (move.IsEnPassant())
-                        numberOfEnpassants++;
-                    if(move.IsPromotion())
-                        numberOfPromotions++;
-                    if(move.IsCastleOO())
-                        numberOfOOCastles++;
-                    if (move.IsCastleOOO())
-                        numberOfOOOCastles++;
-                    if (gameState.IsCurrentSideKingAttacked(_moveData))
-                        numberOfChecks++;
-                    if (move.GetCapturedPiece() == MoveUtility.WhiteKing || move.GetCapturedPiece() == MoveUtility.BlackKing)
-                        checkmates++;
-                }
-                  
+                    
+                    if (depth == 1)
+                    {
+                        if (move.IsPieceCaptured())
+                            numberOfCaptures++;
+                        if (move.IsEnPassant())
+                            numberOfEnpassants++;
+                        if (move.IsPromotion())
+                            numberOfPromotions++;
+                        if (move.IsCastleOO())
+                            numberOfOOCastles++;
+                        if (move.IsCastleOOO())
+                            numberOfOOOCastles++;
+                        if (gameState.IsCurrentSideKingAttacked(_moveData))
+                            numberOfChecks++;
+                    }
+                   
+                }                 
 
                 gameState.UnMakeMove(move);
             }
             return count;
         }
 
+
+        private string CalculateDivide(GameState gameState, int ply, int depth)
+        {
+            var sb = new StringBuilder(DivideOutput);
+            ulong count = 0;
+
+            gameState.GenerateMoves(MoveGenerationMode.All, ply, _moveData);
+
+            foreach (var move in gameState.Moves[ply])
+            {
+                gameState.MakeMove(move);
+
+                if (!gameState.IsOppositeSideKingAttacked(_moveData))
+                {
+                    ulong moveCount = RunPerftRecursively(gameState, ply + 1, depth - 1);
+                    sb.AppendFormat("{0}{1} {2}{3}", MoveUtility.RankAndFile[move.GetFromMove()], MoveUtility.RankAndFile[move.GetToMove()], moveCount, Environment.NewLine);
+                    count += moveCount;                   
+                }
+                  
+
+                gameState.UnMakeMove(move);
+            }
+            sb.AppendFormat("Total Nodes: {0}", count);
+            return sb.ToString();
+            
+        }
     }
 }
