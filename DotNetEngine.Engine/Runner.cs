1bba4c4732f75d63724eee5cd6ad5b4673285f66
Aaron Mell <aaronamell@gmail.com>
2014-10-18 20:48:14 -0500

    Created a new Class that serves as a shim between the engine and the console application. This will make it easier to test the runner.

---
+++ DotNetEngine.Engine/Runner.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Linq;
+using Common.Logging;
+using DotNetEngine.Engine.Objects;
+using JetBrains.Annotations;
+
+namespace DotNetEngine.Engine
+{
+    public class Runner
+    {
+        private const string EngineId = "id name DotNetEngine {0}";
+        private const string AuthorId = "id author Aaron A Mell";
+        private bool _newGame;
+
+        private static readonly Version _version = System.Reflection.Assembly.GetExecutingAssembly()
+            .GetName()
+            .Version;
+
+        private static Engine _engine;
+
+        internal static ILog Logger { get; private set; }
+
+        public Runner([CanBeNull] ILog logger)
+        {
+            _engine = new Engine();
+            _engine.BestMoveFound += _engine_BestMoveFound;
+
+            _engine.NewGame();
+
+            Logger = logger ?? LogManager.GetCurrentClassLogger();
+        }
+
+        public void ProcessCommand(string command, string[] commandArguments)
+        {
+            switch (command.ToUpper())
+            {
+                case "SETBOARD":
+                    {
+                        try
+                        {
+                            _engine.SetBoard(commandArguments.First());
+                        }
+                        catch (Exception)
+                        {
+                             Logger.ErrorFormat("Unable to Load FEN. FEN is not valid");
+                        }
+                        break;
+                    }
+                case "PERFT":
+                    {
+
+
+                        byte depth;
+                        var canParse = byte.TryParse(commandArguments.First(), out depth);
+
+                        if (!canParse)
+                        {
+                            Logger.ErrorFormat("Depth is not a valid value");
+                            break;
+                        }
+                        _engine.Perft(depth);
+                        break;
+                    }
+                case "DIVIDE":
+                    {
+                        byte depth;
+                        var canParse = byte.TryParse(commandArguments.First(), out depth);
+
+                        if (!canParse)
+                        {
+                            Logger.ErrorFormat("Depth is not a valid value");
+                        }
+
+                        _engine.Divide(depth);
+                        break;
+                    }
+                case "UCI":
+                {
+                    Logger.InfoFormat(EngineId, _version);
+                    Logger.InfoFormat(AuthorId);
+                    Logger.InfoFormat("uciok");
+                   
+                    break;
+                }
+                case "ISREADY":
+                {
+                    Logger.InfoFormat("readyok");
+                    break;
+                }
+                case "POSITION":
+                    {
+
+                        if (_newGame)
+                        {
+                            if (commandArguments[0] == "fen")
+                            {
+                                var fen = string.Format("{0} {1} {2} {3} {4} {5}", commandArguments[1], commandArguments[2],
+                               commandArguments[3], commandArguments[4], commandArguments[5], commandArguments[6]);
+
+                                _engine.NewGame(fen);
+                            }
+
+                            var movesIndex = Array.IndexOf(commandArguments, "moves");
+
+                            for (int i = movesIndex + 1; i <= commandArguments.Length - 1; i++)
+                            {
+                                _engine.TryMakeMove(commandArguments[i]);
+                            }
+                        }
+                        else
+                        {
+                            _engine.TryMakeMove(commandArguments[commandArguments.Length - 1]);
+                        }
+
+                        _newGame = false;
+                        break;
+                    }
+                case "UCINEWGAME":
+                    {
+                        _engine = new Engine();
+                        _engine.NewGame();
+                        _engine.BestMoveFound += _engine_BestMoveFound;
+
+                        _newGame = true;
+                        break;
+                    }
+                case "STOP":
+                    {
+                        _engine.Stop();
+                        break;
+                    }
+                case "GO":
+                    {
+                        SetEngineMoveParameters(commandArguments);
+                        _engine.Calculate();
+                        break;
+                    }
+                default:
+                    {
+                        Logger.ErrorFormat("Invalid Command Entered");
+                        break;
+                    }
+            }
+        }
+
+        private static void SetEngineMoveParameters(string[] commandArguments)
+        {
+            _engine.InfiniteTime = false;
+            _engine.CalculateToDepth = -1;
+            _engine.MoveTime = -1;
+            _engine.MateDepth = -1;
+            _engine.MaxNodes = -1;
+            _engine.WhiteTime = -1;
+            _engine.BlackTime = -1;
+            _engine.WhiteIncrementTime = -1;
+            _engine.BlackIncrementTime = -1;
+            _engine.MovesUntilNextTimeControl = -1;
+
+            if (commandArguments.Any(x => x == "infinite"))
+            {
+                _engine.InfiniteTime = true;
+            }
+
+            var depthIndex = Array.IndexOf(commandArguments, "depth");
+
+            if (depthIndex != -1)
+            {
+                _engine.CalculateToDepth = int.Parse(commandArguments[depthIndex + 1]);
+            }
+
+            var moveTimeIndex = Array.IndexOf(commandArguments, "moveTime");
+
+            if (moveTimeIndex != -1)
+            {
+                _engine.MoveTime = int.Parse(commandArguments[moveTimeIndex + 1]);
+            }
+
+            var mateDepthIndex = Array.IndexOf(commandArguments, "mate");
+
+            if (mateDepthIndex != -1)
+            {
+                _engine.MateDepth = int.Parse(commandArguments[mateDepthIndex + 1]);
+            }
+
+            var maxNodesIndex = Array.IndexOf(commandArguments, "nodes");
+
+            if (maxNodesIndex != -1)
+            {
+                _engine.MaxNodes = int.Parse(commandArguments[maxNodesIndex + 1]);
+            }
+
+            var whiteTimeIndex = Array.IndexOf(commandArguments, "wtime");
+
+            if (whiteTimeIndex != -1)
+            {
+                _engine.WhiteTime = int.Parse(commandArguments[whiteTimeIndex + 1]);
+            }
+
+            var blackTimeIndex = Array.IndexOf(commandArguments, "btime");
+
+            if (blackTimeIndex != -1)
+            {
+                _engine.BlackTime = int.Parse(commandArguments[blackTimeIndex + 1]);
+            }
+
+            var whiteIncrementTimeIndex = Array.IndexOf(commandArguments, "winc");
+
+            if (whiteIncrementTimeIndex != -1)
+            {
+                _engine.WhiteIncrementTime = int.Parse(commandArguments[whiteIncrementTimeIndex + 1]);
+            }
+
+            var blackIncrementTimeIndex = Array.IndexOf(commandArguments, "binc");
+
+            if (blackIncrementTimeIndex != -1)
+            {
+                _engine.BlackIncrementTime = int.Parse(commandArguments[blackIncrementTimeIndex + 1]);
+            }
+
+            var movesToGoIndex = Array.IndexOf(commandArguments, "movestogo");
+
+            if (movesToGoIndex != -1)
+            {
+                _engine.MovesUntilNextTimeControl = int.Parse(commandArguments[movesToGoIndex + 1]);
+            }
+        }
+
+        static void _engine_BestMoveFound(object sender, BestMoveFoundEventArgs e)
+        {
+            Logger.InfoFormat("bestmove {0}", e.BestMove);
+        }
+    }
+}
--- DotNetEngine.EngineRunner/Program.cs
+++ DotNetEngine.EngineRunner/Program.cs
@@ -1,27 +1,16 @@
 ﻿using System;
-using System.Linq;
-using DotNetEngine.Engine.Objects;
+using DotNetEngine.Engine;
 
 namespace DotNetEngine.EngineRunner
 {
 	class Program
 	{
-	    private const string EngineId = "id name DotNetEngine {0}";
-        private const string AuthorId = "id author Aaron A Mell";
-
-        private static Engine.Engine _engine;
+	    
 
 		private static void Main()
 		{
-			_engine= new Engine.Engine();
-            _engine.BestMoveFound += _engine_BestMoveFound;
+		    var runner = new Runner(null);
 
-            _engine.NewGame();
-            var version = System.Reflection.Assembly.GetExecutingAssembly()
-                                           .GetName()
-                                           .Version
-                                           .ToString();
-		    bool newGame = false;
 			while (true)
 			{
 
@@ -41,214 +30,14 @@ namespace DotNetEngine.EngineRunner
                 else
                 {
                     command = rawCommand.Substring(0, rawCommand.IndexOf(' '));
-                }                
-
-                switch (command.ToUpper())
-                {
-                    case "SETBOARD":
-                    {
-                        try
-                        {
-                            _engine.SetBoard(commandArguments);                            
-                        }
-                        catch (Exception)
-                        {
-                            Console.WriteLine("Unable to Load FEN. FEN is not valid");
-                        }                       
-                        break;
-                    }
-                    case "PERFT":
-	                {
-		               
-						
-                        byte depth;
-                        var canParse = byte.TryParse(commandArguments, out depth);
-
-                        if (!canParse)
-                        {
-                            Console.WriteLine("Depth is not a valid value");
-                            break;
-                        }
-                        _engine.Perft(depth);
-                        break;
-                    }
-                    case "DIVIDE":
-                    {                       
-                        byte depth;
-                        var canParse = byte.TryParse(commandArguments, out depth);
-
-                        if (!canParse)
-                        {
-                            Console.WriteLine("Depth is not a valid value");
-                            break;
-                        }
-
-                        _engine.Divide(depth);
-                        break;
-                    }
-                    case "UCI":
-                    {
-                        Console.WriteLine(EngineId, version);
-                        Console.WriteLine(AuthorId);
-                        Console.WriteLine("uciok");
-                        break;
-                    }
-                    case "QUIT":
-                    {
-                        return;
-                    }
-                    case "ISREADY":
-                    {
-                        Console.WriteLine("readyok");
-                        break;
-                    }
-                    case "POSITION":
-                    {
-                        var parameters = commandArguments.Split(' ');
-
-                        if (newGame)
-                        {
-                            if (parameters[0] == "fen")
-                            {
-                                var fen = string.Format("{0} {1} {2} {3} {4} {5}", parameters[1], parameters[2],
-                               parameters[3], parameters[4], parameters[5], parameters[6]);
-
-                                _engine.NewGame(fen);
-                            }
-
-                            var movesIndex = Array.IndexOf(parameters, "moves");
-
-                            for (int i = movesIndex + 1; i <= parameters.Length - 1; i++)
-                            {
-                                _engine.TryMakeMove(parameters[i]);
-                            }
-                        }
-                        else
-                        {
-                            _engine.TryMakeMove(parameters[parameters.Length - 1]);
-                        }
-
-                        newGame = false;
-                        break;
-                    }
-                    case "UCINEWGAME":
-                    {
-                        _engine = new Engine.Engine();
-                        _engine.NewGame();
-                        _engine.BestMoveFound += _engine_BestMoveFound;
-                        
-                        newGame = true;
-                        break;
-                    }
-                    case "STOP":
-                    {
-                        _engine.Stop();
-                        break;
-                    }
-                    case "GO":
-                    {
-                        SetEngineMoveParameters(_engine, commandArguments);
-                        _engine.Calculate();
-                        break;
-                    }
-                    default:
-                    {
-                        Console.WriteLine("Invalid Command Entered");
-                        break;
-                    }
                 }
+
+			    if (command.ToUpper() == "QUIT")
+			        return;
+
+			    runner.ProcessCommand(command, commandArguments.Split(' '));
                 Console.WriteLine();
 			}
 		}
-
-        private static void SetEngineMoveParameters(Engine.Engine engine, string commandArguments)
-        {
-            engine.InfiniteTime = false;
-            engine.CalculateToDepth = -1;
-            engine.MoveTime = -1;
-            engine.MateDepth = -1;
-            engine.MaxNodes = -1;
-            engine.WhiteTime = -1;
-            engine.BlackTime = -1;
-            engine.WhiteIncrementTime = -1;
-            engine.BlackIncrementTime = -1;
-            engine.MovesUntilNextTimeControl = -1;
-           
-            var parameters = commandArguments.Split(' ');
-            
-            if (parameters.Any(x => x == "infinite"))
-            {
-                engine.InfiniteTime = true;
-            }
-
-            var depthIndex = Array.IndexOf(parameters, "depth");
-
-            if (depthIndex != -1)
-            {
-                engine.CalculateToDepth = int.Parse(parameters[depthIndex + 1]);
-            }
-
-            var moveTimeIndex = Array.IndexOf(parameters, "moveTime");
-
-            if (moveTimeIndex != -1)
-            {
-                engine.MoveTime = int.Parse(parameters[moveTimeIndex + 1]);
-            }
-
-            var mateDepthIndex = Array.IndexOf(parameters, "mate");
-
-            if (mateDepthIndex != -1)
-            {
-                engine.MateDepth = int.Parse(parameters[mateDepthIndex + 1]);
-            }
-
-            var maxNodesIndex = Array.IndexOf(parameters, "nodes");
-
-            if (maxNodesIndex != -1)
-            {
-                engine.MaxNodes = int.Parse(parameters[maxNodesIndex + 1]);
-            }
-
-            var whiteTimeIndex = Array.IndexOf(parameters, "wtime");
-
-            if (whiteTimeIndex != -1)
-            {
-                engine.WhiteTime = int.Parse(parameters[whiteTimeIndex + 1]);
-            }
-
-            var blackTimeIndex = Array.IndexOf(parameters, "btime");
-
-            if (blackTimeIndex != -1)
-            {
-                engine.BlackTime = int.Parse(parameters[blackTimeIndex + 1]);
-            }
-
-            var whiteIncrementTimeIndex = Array.IndexOf(parameters, "winc");
-
-            if (whiteIncrementTimeIndex != -1)
-            {
-                engine.WhiteIncrementTime = int.Parse(parameters[whiteIncrementTimeIndex + 1]);
-            }
-
-            var blackIncrementTimeIndex = Array.IndexOf(parameters, "binc");
-
-            if (blackIncrementTimeIndex != -1)
-            {
-                engine.BlackIncrementTime = int.Parse(parameters[blackIncrementTimeIndex + 1]);
-            }
-
-            var movesToGoIndex = Array.IndexOf(parameters, "movestogo");
-
-            if (movesToGoIndex != -1)
-            {
-                engine.MovesUntilNextTimeControl = int.Parse(parameters[movesToGoIndex + 1]);
-            }
-
-        }
-
-        static void _engine_BestMoveFound(object sender, BestMoveFoundEventArgs e)
-        {
-            Console.WriteLine("bestmove {0}", e.BestMove);
-        }
 	}
 }
