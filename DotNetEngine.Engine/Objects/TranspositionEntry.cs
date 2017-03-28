77592c2b6617a1eebb29866c0f5c48d9f649acd9
Aaron Mell <aaronamell@gmail.com>
2017-03-27 20:29:23 -0500

    Checking in missing file

---
+++ DotNetEngine.Engine/Objects/TranspositionEntry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetEngine.Engine.Objects
+{
+    /// <summary>
+    /// An Entry in the Transpostion Table
+    /// </summary>
+    internal class TranspositionEntry
+    {
+        /// <summary>
+        /// The best move
+        /// </summary>
+        internal uint Move { get; set; }
+        /// <summary>
+        /// The best score
+        /// </summary>
+        internal int Score { get; set; }
+        /// <summary>
+        /// The depth this entry was added at
+        /// </summary>
+        internal int Depth { get; set; }
+        /// <summary>
+        /// The type of result 0 = Exact, 1 = LowerBound, 2 = UpperBound
+        /// </summary>
+        internal int Flag { get; set; }
+    }
+}
